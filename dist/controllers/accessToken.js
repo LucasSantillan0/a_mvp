b03f271a7e7e0221419c61808d379994b744dc22
lucas <lukitas6641@gmail.com>
2023-12-10 21:21:22 -0300

    first

--- dist/controllers/accessToken.js
+++ dist/controllers/accessToken.js
@@ -1,21 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.saveAccessToken = void 0;
+var mercadopago_1 = require("mercadopago");
 exports.saveAccessToken = function (req, res) {
     var query = req.query;
     console.log(query);
-    // const client = new MercadoPagoConfig({ accessToken: 'access_token', options: { timeout: 5000 } });
-    // 
-    // const oauth = new OAuth(client);
-    //
-    // oauth.create({
-    //     body: {
-    //         'client_secret': '6bnJQjxhFs',
-    //         'client_id': 'TESTUSER925637351',
-    //         'code': query.code as string,
-    //         'redirect_uri': 'redirect-uri'
-    //     }
-    // }).then((result) => console.log(result))
-    //     .catch((error) => console.log(error));
+    var client = new mercadopago_1.MercadoPagoConfig({ accessToken: 'access_token', options: { timeout: 5000 } });
+    var oauth = new mercadopago_1.OAuth(client);
+    oauth.create({
+        body: {
+            'client_secret': '6bnJQjxhFs',
+            'client_id': 'TESTUSER925637351',
+            'code': query.code,
+            'redirect_uri': 'https://a-q9bs8kfh2-lucassantillan0.vercel.app'
+        }
+    }).then(function (result) { return console.log(result); })
+        .catch(function (error) { return console.log(error); });
     res.send("ok");
 };
--- src/controllers/accessToken.ts
+++ src/controllers/accessToken.ts
@@ -5,18 +5,18 @@ export const saveAccessToken = (req: Request, res: Response) => {
     const { query } = req
     console.log(query)
 
-   // const client = new MercadoPagoConfig({ accessToken: 'access_token', options: { timeout: 5000 } });
-   // 
-   // const oauth = new OAuth(client);
-//
-   // oauth.create({
-   //     body: {
-   //         'client_secret': '6bnJQjxhFs',
-   //         'client_id': 'TESTUSER925637351',
-   //         'code': query.code as string,
-   //         'redirect_uri': 'redirect-uri'
-   //     }
-   // }).then((result) => console.log(result))
-   //     .catch((error) => console.log(error));
+ const client = new MercadoPagoConfig({ accessToken: 'access_token', options: { timeout: 5000 } });
+ 
+ const oauth = new OAuth(client);
+
+ oauth.create({
+     body: {
+         'client_secret': '6bnJQjxhFs',
+         'client_id': 'TESTUSER925637351',
+         'code': query.code as string,
+         'redirect_uri': 'https://a-q9bs8kfh2-lucassantillan0.vercel.app'
+     }
+ }).then((result) => console.log(result))
+     .catch((error) => console.log(error));
    res.send("ok")
 }
